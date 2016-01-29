e56196b453c0976b7ab31269631e722f916538dc
inuscript <inuscript@gmail.com>
2016-01-29 22:45:38 +0900

    remove arrow function

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 var assert = require("assert")
 var sample = require("./sample")
 var parse = require("../index")
-describe("parse", () => {
+describe("parse", function() {
   var expect = [ 
     { top: 0, left: 0, color: { r: 255, g: 13, b: 0, a: 255 } },
     { top: 0, left: 1, color: { r: 0, g: 255, b: 0, a: 255 } },
@@ -12,7 +12,7 @@ describe("parse", () => {
     { top: 1, left: 2, color: { r: 166, g: 225, b: 45, a: 255 } },
     { top: 1, left: 3, color: { r: 230, g: 219, b: 115, a: 171 } } 
   ]
-  it("test", (done) => {
+  it("test", function(done){
     sample( function(err, res) {
       var result = parse(res)
       assert.deepEqual(result, expect)
--- test/sample.js
+++ test/sample.js
@@ -1,5 +1,5 @@
 var pngparse = require("pngparse")
 var fs = require("fs")
-module.exports = (cb) => {
+module.exports = function(cb) {
   pngparse.parse(fs.readFileSync("./test/sample.png"),cb)
 }
